8e7d5139492e38347aa26fa132a84ad77c1c0b7f
kashitaka <makkausu@gmail.com>
2025-05-25 03:57:54 +0900

    init next blog template

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },  
+  distDir: 'docs',
+});
