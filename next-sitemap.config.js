c1f288a695106f482b59d7412cd63ea2d3498674
kashitaka <makkausu@gmail.com>
2025-06-24 00:38:43 +0900

    change robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,13 @@ module.exports = {
   siteUrl: "https://kashitaka.dev",
   generateRobotsTxt: true,
   outDir: './docs',
+  robotsTxtOptions: {
+    transformRobotsTxt: async (_, robotsTxt) => {
+			const withoutHost = robotsTxt.replace(
+				`# Host\nHost: https://kashitaka.dev\n\n`,
+				"",
+			);
+			return withoutHost;
+		},
+  },
 }
