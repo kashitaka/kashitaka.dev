662c2a52e79e49432c821e2f3515e71ce0d44dce
kashitaka <makkausu@gmail.com>
2025-11-24 01:00:14 +0900

    add post: uzbek day2

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,17 +1,39 @@
 import { remark } from "remark"
 import remarkGfm from "remark-gfm"
 import remarkRehype from "remark-rehype"
+import rehypeRaw from "rehype-raw"
+import rehypeSanitize from "rehype-sanitize"
+import { defaultSchema } from "hast-util-sanitize"
 import rehypeExternalLinks from "rehype-external-links"
 import rehypeHighlight from "rehype-highlight"
 import rehypeStringify from "rehype-stringify"
 
 export default async function markdownToHtml(markdown: string) {
+  const schema = {
+    ...defaultSchema,
+    tagNames: [...(defaultSchema.tagNames || []), "iframe"],
+    attributes: {
+      ...defaultSchema.attributes,
+      iframe: [
+        "src",
+        "width",
+        "height",
+        "frameborder",
+        "allow",
+        "allowfullscreen",
+        "loading",
+      ],
+    },
+  }
+
   const result = await remark()
-    .use(remarkGfm)
-    .use(remarkRehype)
-    .use(rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] })
-    .use(rehypeHighlight)
-    .use(rehypeStringify)
-    .process(markdown)
+      .use(remarkGfm)
+      .use(remarkRehype, { allowDangerousHtml: true })
+      .use(rehypeRaw)
+      .use(rehypeSanitize, schema)
+      .use(rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer"] })
+      .use(rehypeHighlight)
+      .use(rehypeStringify)
+      .process(markdown)
   return result.toString()
 }
